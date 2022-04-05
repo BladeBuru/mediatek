4fe0e8a6ec3392d5ca712ef3713f619a0d7dc959
BladeBuru <72576887+BladeBuru@users.noreply.github.com>
2022-04-05 17:18:26 +0200

    Appli

---
+++ persistance/MediathequeData.java
@@ -0,0 +1,46 @@
+package persistance;
+
+import java.util.List;
+import mediatek2022.*;
+
+// classe mono-instance  dont l'unique instance est connue de la médiatheque
+// via une auto-déclaration dans son bloc static
+
+public class MediathequeData implements PersistentMediatheque {
+// Jean-François Brette 01/01/2018
+	static {
+		Mediatheque.getInstance().setData(new MediathequeData());
+	}
+
+	private MediathequeData() {
+	}
+
+	// renvoie la liste de tous les documents disponibles de la médiathèque
+	@Override
+	public List<Document> tousLesDocumentsDisponibles() {
+		return null;
+	}
+
+	// va récupérer le User dans la BD et le renvoie
+	// si pas trouvé, renvoie null
+	@Override
+	public Utilisateur getUser(String login, String password) {
+		return null;
+	}
+
+	// va récupérer le document de numéro numDocument dans la BD
+	// et le renvoie
+	// si pas trouvé, renvoie null
+	@Override
+	public Document getDocument(int numDocument) {
+		return null;
+	}
+
+	@Override
+	public void ajoutDocument(int type, Object... args) {
+		// args[0] -> le titre
+		// args [1] --> l'auteur
+		// etc... variable suivant le type de document
+	}
+
+}
